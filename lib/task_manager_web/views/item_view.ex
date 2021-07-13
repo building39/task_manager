448db80b2045304395d02fe99914ff1b73f32afb
MichaelMartin <mmartin@korcett.com>
2021-07-13 10:19:58 -0400

    Add complete and checked functions

--- lib/task_manager_web/views/item_view.ex
+++ lib/task_manager_web/views/item_view.ex
@@ -1,3 +1,19 @@
 defmodule TaskManagerWeb.ItemView do
   use TaskManagerWeb, :view
+
+  # add class "completed" to a list item if item.status=1
+  def complete(item) do
+    case item.status do
+      1 -> "completed"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
+
+  # add "checked" to input if item.status=1
+  def checked(item) do
+    case item.status do
+      1 -> "checked"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
 end
